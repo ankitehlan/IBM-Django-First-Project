aa4849a7310d963fd7837260447cd71b291bf024
Ankit Tehlan <ankitehlan@gmail.com>
2023-10-22 22:26:14 +0530

    Add models and admin logic

--- firstproject/firstapp/admin.py
+++ firstproject/firstapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Course, Instructor, Lesson
+
 # Register your models here.
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 3
+    
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+    inlines = [LessonInline]
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Instructor)
+
